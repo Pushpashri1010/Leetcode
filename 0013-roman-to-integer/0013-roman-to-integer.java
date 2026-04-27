97da77d0c56c45c6079b1c531c75a990fee07a58
Pushpashri1010 <pushpa101005@gmail.com>
2026-04-27 09:32:57 +0530

    Time: 6 ms (8.59%), Space: 47.2 MB (6.83%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,60 @@
+// class Solution {
+//     public int romanToInt(String s) {
+//          int n[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+//          String str[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+//          int res=0;
+//          int i=0,j=0;
+//          while(i<s.length()){
+//             String s1=s.charAt(i);
+//             String s2=s1+s.charAt(i+1);
+//             if(s2==str[j]){
+//                 res+=n[j];
+//                 i=i+2;
+//             }
+//             else if(s1==str[j]){
+//                 res+=n[j];
+//                 i=i+1;
+//             }
+//             else{
+//                 j++;
+//             }
+
+//          }
+//          return res;
+//     }
+// }
+
+
+
+class Solution {
+    public int romanToInt(String s) {
+        int n[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String str[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        int res = 0;
+        int i = 0, j = 0;
+
+        while (i < s.length()) {
+            // Check 2-character match safely
+            if (i + 1 < s.length()) {
+                String s2 = s.substring(i, i + 2);
+                if (s2.equals(str[j])) {
+                    res += n[j];
+                    i += 2;
+                    continue;
+                }
+            }
+
+            // Check 1-character match
+            String s1 = s.substring(i, i + 1);
+            if (s1.equals(str[j])) {
+                res += n[j];
+                i += 1;
+            } else {
+                j++;
+            }
+        }
+
+        return res;
+    }
+}
